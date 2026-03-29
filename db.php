e271ada194a91f69bb37151b63ef1c25a70466e5
Samuel Pacris II <samuelpacris5@gmail.com>
2026-03-29 23:35:49 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$mysqli_hostname = "localhost";
+$mysqli_user = "root";
+$mysqli_password = "";
+$mysqli_database = "liveedit";
+$bd = mysqli_connect($mysqli_hostname, $mysqli_user, $mysqli_password) 
+or die("Ooopps! Something went wrong.");
+
+?>
